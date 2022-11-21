15c3910e5983d676fa363fd5a3378c1389bbea80
ABINAYA <727721euit002@skcet.ac.in>
2022-11-21 15:58:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import "./App.css";
+const App = () => {
+  return (
+    <div className="statistics">
+      <div className="image-13">
+        <div className="flex-container">
+          <span className="home">HOME </span>
+          <span className="services">SERVICES </span>
+          <span className="about-us">ABOUT US </span>
+          <span className="contact"> CONTACT</span>
+        </div>
+        <div className="flex-container-1">
+          <div className="rectangle-293">
+            <span className="num-2019">2019</span>
+          </div>
+          <div className="rectangle-292">
+            <span className="num-2020">2020</span>
+          </div>
+          <div className="rectangle-245">
+            <span className="statistics-1">Statistics</span>
+          </div>
+        </div>
+        <div className="rectangle-291">
+          <span className="every-day-is-earth-d">
+            Every day is Earth Day When you work in Agriculture
+          </span>
+        </div>
+        <div className="flex-container-2">
+          <button className="rectangle-295">
+            <span className="num-2021">2021</span>
+          </button>
+          <div className="rectangle-294">
+            <span className="num-2022">2022</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default App;
